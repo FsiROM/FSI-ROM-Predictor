7c651fa5ce06c4746aaeec487bef65e7c322797c
Azzeddine TIBA <azzeddine.tiba@gmail.com>
2024-09-26 11:58:32 +0200

    Update tutos

--- examples/twoFsiFlaps_Re_300/MainKratos.py
+++ examples/twoFsiFlaps_Re_300/MainKratos.py
@@ -1,11 +1,6 @@
 import KratosMultiphysics as KM
 from KratosMultiphysics.CoSimulationApplication.co_simulation_analysis import CoSimulationAnalysis
 
-"""
-For user-scripting it is intended that a new class is derived
-from CoSimulationAnalysis to do modifications
-Check also "kratos/python_scripts/analysis-stage.py" for available methods that can be overridden
-"""
 
 parameter_file_name = "DoubleFlap_fsi_parameters_ROM.json"
 with open(parameter_file_name,'r') as parameter_file:
